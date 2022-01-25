7ef34cfb6166cc43a7f61b0fc1cebf344c632f78
FelineJTD <16520335@std.stei.itb.ac.id>
2022-01-25 23:07:37 +0700

    add comments

--- wordsearch.cpp
+++ wordsearch.cpp
@@ -5,13 +5,19 @@
 using namespace std;
 
 void read_file(string filename, vector<vector<char>>& board, vector<string>& words) {
+  /* I.S. filename terdefinisi, board dan words sembarang
+     F.S. board berisi matriks puzzle dan words berisi array kata-kata yang diambil dari file bernama filename
+  */
+  /* KAMUS */
+  /* ALGORITMA */
+  // buka file
   ifstream input;
   input.open(filename, ios::in);
-  if (!input) {
+  if (!input) { // file tidak ada
     cout << "No such file";
-  } else {
+  } else { // file ada
+    // baca file
     string line;
-
     // board
     while (getline(input, line) && line !="") {
       vector<char> current_line;
@@ -23,7 +29,6 @@ void read_file(string filename, vector<vector<char>>& board, vector<string>& wor
       }
       board.push_back(current_line);
     }
-
     // words
     while (getline(input, line)) {
       words.push_back(line);
@@ -32,26 +37,41 @@ void read_file(string filename, vector<vector<char>>& board, vector<string>& wor
 }
 
 void check_surrounding(int i, int j, vector<vector<char>> board, string word, int &cmp, int &total) {
+  /* I.S. i dan j terdefinisi, berupa koordinat posisi sekarang,
+          board terdefinisi, berupa puzzle,
+          word terdefinisi, berupa kata yang sedang dicari,
+          cmp dan total terdefinisi, berupa total perbandingan kata dan total kata yang telah ditemukan
+     F.S. bila word ditemukan, jawaban dicetak,
+          cmp bertambah sebanyak jumlah perbandingan yang dilakukan,
+          total bertambah 1 bila word ditemukan
+  */
+  /* KAMUS */
   bool found = false;
   int m = board.size();
   int n = board[0].size();
   int w = word.size();
+  /* ALGORITMA */
+  // iterasi sekeliling koordinat (i dari -1 hingga 1 dan j dari -1 hingga 1 relatif terhadap koordinat awal)
   for (int row = -1; row <= 1; row++) {
     for (int column = -1; column <= 1; column++) {
-      if (!(row == 0 && column == 0)) { // not itself
-        // check if out of bounds
-        if ((-1<=i+(w*row)) && (-1<=j+(w*column)) && ((i+w*row)<=m) && ((j+w*column)<=n)) {
+      // bila merupakan koordinat awal (0 dan 0 relatif dari koordinat awal), huruf tidak diperiksa
+      if (!(row == 0 && column == 0)) { 
+        // bila diprediksi out of bounds, huruf tidak diperiksa
+        if ((-1<=i+(w*row)) && (-1<=j+(w*column)) && ((i+w*row)<=m) && ((j+w*column)<=n)) { 
           found = true;
+          // lakukan perbandingan huruf per huruf dari huruf kedua kata, sambil menambahkan cmp
           for (int letter=1; letter<word.size(); letter++) {
             cmp += 1;
-            //cout << board[i+(row*letter)][j+(column*letter)] << "\n";
+            // bila huruf tidak sesuai, keluar dari loop dan lanjutkan pencarian ke arah lain
             if (board[i+(row*letter)][j+(column*letter)] != word[letter]) {
               found = false;
               break;
             }
           }
+          // bila kata ditemukan, cetak jawaban dan tambahkan total dengan 1
           if (found) {
             total += 1;
+            // buat matriks sementara untuk menampung solusi
             char solution[m][n];
             for (int x=0; x<m; x++) {
               for (int y=0; y<n; y++) {
@@ -61,7 +81,7 @@ void check_surrounding(int i, int j, vector<vector<char>> board, string word, in
             for (int letter=0; letter<word.size(); letter++) {
               solution[i+(row*letter)][j+(column*letter)] = word[letter];
             }
-            // print solution
+            // cetak matriks solusi
             cout << word << "\n";
             for (int x=0; x<m; x++) {
               for (int y=0; y<n; y++) {
